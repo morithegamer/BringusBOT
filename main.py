70301e0fa8606345a58973377781aa8224a5c3c4
Mori <55035219+morithegamer@users.noreply.github.com>
2025-04-28 19:22:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
