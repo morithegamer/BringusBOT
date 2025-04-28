5b9d628177764497ceb34db0326691f2dbb962df
Mori <55035219+morithegamer@users.noreply.github.com>
2025-04-28 02:14:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Bringus Bot Starter
+# Bringus Bot Main Starter
 import discord
 from discord.ext import commands
 import os
@@ -12,11 +12,13 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="over the Velvet Room ✨"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Velvet Travelers arrive..."))
     await bot.tree.sync()
+    from cogs.bringus_verify import VerifyView
+    bot.add_view(VerifyView())
 
     for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
+        if filename.endswith('.py') and filename != "__init__.py":
             await bot.load_extension(f'cogs.{filename[:-3]}')
 
 bot.run("MTM0NjI1MTQ1NTc2NTU0OTEzOA.GvAIcI.S-zhk5fQyt2QRv-EjgWJF88r-mtNeYABg2xtEk")
