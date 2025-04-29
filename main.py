7e2a84f28ca4c41074c9e00f036d9d93ab566492
Mori <55035219+morithegamer@users.noreply.github.com>
2025-04-28 20:07:42 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Bringus Bot Main Starter
 import discord
 from discord.ext import commands
 import os
@@ -12,11 +11,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-    await bot.change_presence(activity=discord.Activity(
-        type=discord.ActivityType.watching,
-        name="Velvet Travelers arrive..."
-    ))
-    
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Velvet Travelers arrive..."))
     try:
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} command(s).")
@@ -29,6 +24,5 @@ async def on_ready():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py') and filename != "__init__.py":
             await bot.load_extension(f'cogs.{filename[:-3]}')
-
 
 bot.run("MTM0NjI1MTQ1NTc2NTU0OTEzOA.GvAIcI.S-zhk5fQyt2QRv-EjgWJF88r-mtNeYABg2xtEk")
